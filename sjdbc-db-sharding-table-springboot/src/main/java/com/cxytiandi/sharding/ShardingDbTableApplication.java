9c6904cc3bd420c98b1851163c58b5f3b4891d49
lichengcan <li929825769@163.com>
2023-06-26 23:04:47 +0800

    Spring Boot版 Sharding JDBC 分库分表案例

--- sjdbc-db-sharding-table-springboot/src/main/java/com/cxytiandi/sharding/ShardingDbTableApplication.java
+++ sjdbc-db-sharding-table-springboot/src/main/java/com/cxytiandi/sharding/ShardingDbTableApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Spring Boot版 Sharding JDBC 分库分表案例
- * 
- * @author yinjihuan
  *
+ * @author yinjihuan
  * @about http://cxytiandi.com/about
  */
 @SpringBootApplication
 public class ShardingDbTableApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ShardingDbTableApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingDbTableApplication.class, args);
+    }
 }
