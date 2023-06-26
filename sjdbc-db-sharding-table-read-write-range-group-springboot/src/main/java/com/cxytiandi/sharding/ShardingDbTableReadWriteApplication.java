22cc33ff5aeedfe6ae000efa3c8df67ef583f667
lichengcan <li929825769@163.com>
2023-06-26 23:04:03 +0800

    Spring Boot版 Sharding JDBC 分库分表+读写分离案例(范围分表+取模=无限扩容)

--- sjdbc-db-sharding-table-read-write-range-group-springboot/src/main/java/com/cxytiandi/sharding/ShardingDbTableReadWriteApplication.java
+++ sjdbc-db-sharding-table-read-write-range-group-springboot/src/main/java/com/cxytiandi/sharding/ShardingDbTableReadWriteApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Spring Boot版 Sharding JDBC 分库分表+读写分离案例(范围分表+取模=无限扩容)
- * 
- * @author yinjihuan
  *
+ * @author yinjihuan
  * @about http://cxytiandi.com/about
  */
 @SpringBootApplication
 public class ShardingDbTableReadWriteApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ShardingDbTableReadWriteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingDbTableReadWriteApplication.class, args);
+    }
 }
