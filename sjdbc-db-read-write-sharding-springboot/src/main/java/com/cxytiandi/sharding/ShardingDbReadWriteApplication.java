fffca7e6ff428f3637525a484292cf1da2c9d387
lichengcan <li929825769@163.com>
2023-06-27 20:58:54 +0800

    1

--- sjdbc-db-read-write-sharding-springboot/src/main/java/com/cxytiandi/sharding/ShardingDbReadWriteApplication.java
+++ sjdbc-db-read-write-sharding-springboot/src/main/java/com/cxytiandi/sharding/ShardingDbReadWriteApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ShardingDbReadWriteApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShardingDbReadWriteApplication.class, args);
 	}
